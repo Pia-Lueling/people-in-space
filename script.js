f72d1981f64716661ac2ed1928dc2884923ed636
Pia Lüling <p.lueling@gmail.com>
2023-08-17 19:00:15 +0200

    write async function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Sanity check");
+
+const spanElement = document.querySelector("[data-js=people-in-space]");
+
+async function peopleInSpace() {
+  const response = await fetch("http://api.open-notify.org/astros.json");
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+peopleInSpace();
